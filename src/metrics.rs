a8a26786cc6215a8497fdd78ea8e3295cdf192eb
David H. Liu <hl7@cs.princeton.edu>
2020-03-08 20:50:12 -0400

    metrics for stat

---
+++ src/metrics.rs
@@ -0,0 +1,31 @@
+use std::collections::{BTreeMap};
+
+#[derive(Clone, Debug)]
+pub struct Metrics {
+    pub start_tsp: u64,
+    pub end_tsp: u64,
+    pub num_drop: u32,  // number of dropped requests
+    pub num_complete: u32,  // number of requests completed
+    pub num_evict: u32, // number of evictions
+    pub vm_mem_size: BTreeMap<usize, usize>,
+    pub boot_tsp: BTreeMap<usize, Vec<u64>>, // key is vm_id, value is boot timestamp
+    pub evict_tsp: BTreeMap<usize, Vec<u64>>,
+    pub req_rsp_tsp: BTreeMap<usize, Vec<u64>> // key is vm_id, value is request send time and response receive time
+}
+
+impl Metrics {
+    pub fn new() -> Metrics {
+        return Metrics {
+            start_tsp:0,
+            end_tsp:0,
+            num_drop: 0,
+            num_complete: 0,
+            num_evict: 0,
+            vm_mem_size: BTreeMap::new(),
+            boot_tsp: BTreeMap::new(),
+            evict_tsp: BTreeMap::new(),
+            req_rsp_tsp: BTreeMap::new(),
+        }
+
+    }
+}
