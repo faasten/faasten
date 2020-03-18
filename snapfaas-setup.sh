5127947a7a71a1a0c8d2bde6403dea26372aa4bd
David H. Liu <hl7@cs.princeton.edu>
2020-03-18 10:46:41 -0400

    move default controller config

--- snapfaas-setup.sh
+++ snapfaas-setup.sh
@@ -4,7 +4,7 @@ sudo mkdir -p /etc/snapfaas
 sudo chown :snapfaas /etc/snapfaas
 sudo chmod 775 /etc/snapfaas
 
-sudo cp ./bins/snapctr/default-conf.yaml /etc/snapfaas
+sudo cp ./resources/default-conf.yaml /etc/snapfaas
 sudo cp ./resources/vmlinux /etc/snapfaas
 sudo cp ./resources/example_function_configs.yaml /etc/snapfaas
 
