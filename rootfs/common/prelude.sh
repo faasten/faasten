2ae095d1758664d62dc617bdc9301defa86483e0
Yue Tan <yuetan@cs.princeton.edu>
2020-10-30 00:56:47 -0500

    clean up and update rootfs/snapfaas and rootfs/fullapp to use postscript and prelude located in common.

---
+++ rootfs/common/prelude.sh
@@ -0,0 +1,22 @@
+apk add openrc util-linux
+
+cp /common/outl /usr/bin/outl
+cp /common/setup-eth0.sh /usr/bin/setup-eth0.sh
+cp /common/ioctl /usr/bin/ioctl
+
+## Add /dev and /proc file systems to openrc's boot
+rc-update add devfs boot
+rc-update add procfs boot
+
+## Create start script for that mounts the appfs and invokes whatever binary is in /srv/workload
+cat /runtime/workload.sh > /bin/workload
+chmod +x /bin/workload
+
+## Have the start script invoked by openrc/init
+printf '#!/sbin/openrc-run\n
+command="/bin/workload"\n' > /etc/init.d/serverless-workload
+chmod +x /etc/init.d/serverless-workload
+rc-update add serverless-workload default
+
+## specify default system library path
+echo '/srv/lib:/lib:/usr/local/lib:/usr/lib' > /etc/ld-musl-$(uname -m).path
