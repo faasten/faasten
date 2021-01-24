c17874c12c148704e9deaec844ce83248575a559
Yue Tan <yuetan@cs.princeton.edu>
2021-01-24 14:16:17 -0600

    add scripts to get execution time scripts

--- microbenchmark/get_exec_time_breakdown.sh
+++ microbenchmark/get_exec_time_breakdown.sh
@@ -2,18 +2,22 @@
 
 source ./default_env
 
-[ ! -d ept_out ] && mkdir ept_out
-for (( i=0; i<100; i++ ))
+# create directories for latency numbers
+[ ! -d fullapp-eager-latency-out ] && mkdir fullapp-eager-latency-out
+[ ! -d snapfaas-eager-latency-out ] && mkdir snapfaas-eager-latency-out
+[ ! -d regular-latency-out ] && mkdir regular-latency-out
+# create directories for ftrace reports
+[ ! -d fullapp-eager-report-out ] && mkdir fullapp-eager-report-out
+[ ! -d snapfaas-eager-report-out ] && mkdir snapfaas-eager-report-out
+[ ! -d regular-report-out ] && mkdir regular-report-out
+for (( i=0; i<1; i++ ))
 do
     echo 'Round' $i
-    >ept_out/ept_$i.txt
     for runtime in python3 nodejs
     do
         for app in $(ls ../snapfaas-images/appfs/$runtime)
         do
-            cat ../resources/requests/$app-$runtime.json | head -1 > tmp.json
-
-            echo "- full-function-eager: $app-$runtime" | tee -a ept_out/ept_$i.txt
+            echo "- full-function-eager: $app-$runtime"
             sudo trace-cmd record -e kvm \
             taskset -c 0 sudo $MEMBINDIR/fc_wrapper \
                 --vcpu_count 1 \
@@ -21,18 +25,17 @@ do
                 --kernel $KERNEL \
                 --network $NETDEV \
                 --firerunner $MEMBINDIR/firerunner \
-                --rootfs $SSDROOTFSDIR/$app-$runtime.ext4 \
+                --rootfs $SSDROOTFSDIR/fullapp/$app-$runtime.ext4 \
                 --load_dir $SSDSNAPSHOTDIR/$app-$runtime \
                 --copy_base \
-                --odirect_base &>/dev/null < tmp.json
-            trace-cmd report 1> ept_report 2>/dev/null
-            trace_cmd_scripts/print_host_latencies.py --ftrace_report ept_report > tmp.out
-            total1=$(cat tmp.out | head -2 | tail -1 | awk '{ print $2 }')
-            echo "total(us): $total1" >> ept_out/ept_$i.txt
-            count1=$(cat tmp.out | head -2 | tail -1 | awk '{ print $3 }')
-            echo "count: $count1" >> ept_out/ept_$i.txt
-
-            echo "- regular boot: $app-$runtime" | tee -a ept_out/ept_$i.txt
+                --odirect_base 1>fullapp-eager-latency-out/$app-$runtime.$i.txt 2>/dev/null < <(cat ../resources/requests/$app-$runtime.json | head -1)
+            trace-cmd report 1> fullapp-eager-report-out/$app-$runtime.$i.txt 2>/dev/null
+            #trace_cmd_scripts/print_host_latencies.py --ftrace_report ept_report > breakdown.$1.txt
+            #total1=$(cat tmp.out | head -2 | tail -1 | awk '{ print $2 }')
+            #echo "total(us): $total1" >> ept_out/ept_$i.txt
+            #count1=$(cat tmp.out | head -2 | tail -1 | awk '{ print $3 }')
+            #echo "count: $count1" >> ept_out/ept_$i.txt
+            echo "- snapfaas-eager: $app-$runtime"
             sudo trace-cmd record -e kvm \
             taskset -c 0 sudo $MEMBINDIR/fc_wrapper \
                 --vcpu_count 1 \
@@ -40,24 +43,36 @@ do
                 --kernel $KERNEL \
                 --network $NETDEV \
                 --firerunner $MEMBINDIR/firerunner \
-                --rootfs $SSDROOTFSDIR/$app-$runtime.ext4 &>/dev/null < tmp.json
-            trace-cmd report 1>ept_report 2>/dev/null
-            # find the line of port IO write 124 to 0x3f0
-            linenum=$(egrep -n 0x3f0 ept_report | head -1 | awk -F: '{ print $1 }')
-            # discard all traces up to the port IO
-            cat ept_report | tail -n +$((linenum + 1)) > ept_report_truncated
-            trace_cmd_scripts/print_host_latencies.py --ftrace_report ept_report_truncated > tmp.out
-            total2=$(cat tmp.out | head -2 | tail -1 | awk '{ print $2 }')
-            echo "total(us): $total2" >> ept_out/ept_$i.txt
-            count2=$(cat tmp.out | head -2 | tail -1 | awk '{ print $3 }')
-            echo "count: $count2" >> ept_out/ept_$i.txt
+                --rootfs $SSDROOTFSDIR/snapfaas/$runtime.ext4 \
+                --appfs $SSDAPPFSDIR/$app-$runtime.ext2 \
+                --load_dir $MEMSNAPSHOTDIR/$runtime \
+                --diff_dirs $SSDSNAPSHOTDIR/diff/$app-$runtime \
+                --copy_diff 1>snapfaas-eager-latency-out/$app-$runtime.$i.txt 2>/dev/null < <(cat ../resources/requests/$app-$runtime.json | head -1)
+            trace-cmd report 1> snapfaas-eager-report-out/$app-$runtime.$i.txt 2>/dev/null
 
-            echo "- difference" >> ept_out/ept_$i.txt
-            echo "total(us): $((total1-total2))" >> ept_out/ept_$i.txt
-            echo "count: $((count1-count2))" >> ept_out/ept_$i.txt
+            echo "- regular boot: $app-$runtime"
+            sudo trace-cmd record -e kvm \
+            taskset -c 0 sudo $MEMBINDIR/fc_wrapper \
+                --vcpu_count 1 \
+                --mem_size 128 \
+                --kernel $KERNEL \
+                --network $NETDEV \
+                --firerunner $MEMBINDIR/firerunner \
+                --rootfs $SSDROOTFSDIR/regular/$app-$runtime.ext4 1>regular-latency-out/$app-$runtime.$i.txt 2>/dev/null < <(cat ../resources/requests/$app-$runtime.json | head -1)
+            trace-cmd report 1>regular-report-out/$app-$runtime.$i.txt 2>/dev/null
+            # find the line of port IO write to 0x3f0
+            #linenum=$(egrep -n 0x3f0 ept_report | head -1 | awk -F: '{ print $1 }')
+            # discard all traces up to the port IO
+            #cat ept_report | tail -n +$((linenum + 1)) > regular-report-out/$app-$runtime.$i.txt
+            #trace_cmd_scripts/print_host_latencies.py --ftrace_report ept_report_truncated > tmp.out
+            #total2=$(cat tmp.out | head -2 | tail -1 | awk '{ print $2 }')
+            #echo "total(us): $total2" >> ept_out/ept_$i.txt
+            #count2=$(cat tmp.out | head -2 | tail -1 | awk '{ print $3 }')
+            #echo "count: $count2" >> ept_out/ept_$i.txt
+
+            #echo "- difference" >> ept_out/ept_$i.txt
+            #echo "total(us): $((total1-total2))" >> ept_out/ept_$i.txt
+            #echo "count: $((count1-count2))" >> ept_out/ept_$i.txt
         done
     done
 done
-
-rm ept_report* tmp.*
-rm -f trace.dat
---
+++ microbenchmark/trace_cmd_scripts/get_exec_time_breakdown.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import print_host_latencies as print_host
+
+#get latencies
+latency_dirs=['../fullapp-eager-latency-out',
+        '../snapfaas-eager-latency-out',
+        '../regular-latency-out']
+
+exec_latencies_all = []
+for d in latency_dirs:
+    files = sorted(os.listdir(d))
+    exec_latencies = {}
+    for f in files:
+        fpath = os.path.join(d, f)
+        with open(fpath) as infile:
+            lines = infile.readlines()
+            app = f.split('.')[0]
+            # in microseconds
+            exec_latencies[app] = int(lines[4].split()[-2])
+    exec_latencies_all.append(exec_latencies)
+
+report_dirs=['../fullapp-eager-report-out',
+        '../snapfaas-eager-report-out',
+        '../regular-report-out']
+with open('breakdown.raw.txt', 'w') as ofile, open('breakdown.txt', 'w') as ofile2:
+    for d, exec_latencies in zip(report_dirs, exec_latencies_all):
+        print(d, file=ofile)
+        print(d, file=ofile2)
+        files = os.listdir(d)
+        for f in files:
+            fpath = os.path.join(d, f)
+            with open(fpath) as infile:
+                # discard all lines before the first write to 0x3f0
+                for line in infile:
+                    if '0x3f0' in line:
+                        break
+                # only regular has a second write tto 0x3f0
+                # discard all lines before this second write
+                if d == report_dirs[-1]:
+                    for line in infile:
+                        if '0x3f0' in line:
+                            break
+                latencies, counts, _, _, _, _ = print_host.parse_report('', infile=infile)
+            app = f.split('.')[0]
+            iotypes = ['EPT_VIOLATION-mmio','EPT_MISCONFIG','HLT']
+            iototal = 0
+            for t in iotypes:
+                iototal += sum(latencies[t])
+            resstr = ','.join([app, 'total', str(exec_latencies[app]), 'I/O', str(iototal), 'page faults', str(sum(latencies['EPT_VIOLATION']))])
+            for k, v in sorted(latencies.items(), key=lambda x: sum(x[1]), reverse=True):
+                if k not in iotypes and k != 'EPT_VIOLATION':
+                    resstr = ','.join([resstr, k, str(sum(v))])
+            print(resstr, file=ofile2)
+
+            resstr = ','.join([app, 'total', str(exec_latencies[app])])
+            for k, v in sorted(latencies.items(), key=lambda x: sum(x[1]), reverse=True):
+                resstr = ','.join([resstr, k, str(sum(v))])
+            print(resstr, file=ofile)
