4a80d864d84e96ae340bb08e5f05c491dc4a7515
Amit Aryeh Levy <amit@amitlevy.com>
2022-04-26 18:53:09 -0400

    Add an immutable blobstore
    Adds a blobstore absrtaction, backed by local files. Unlike the "File
    System" or key-value abstractions, blobs can be written and read
    incrementally (in multiple system calls) rather and have a
    file-descriptor table to name them across system calls.
    
    The blobstore uses content-addressable names (SHA256 hashes) to name
    blobs. Importantly, blobs are immutable once created (using the
    `finalize` system call) and names are undiscoverable, so no security
    policy need apply to blobs directly. If a client can name a blob, they
    can access its content.

---
+++ snapfaas/src/blobstore/mod.rs
@@ -0,0 +1,110 @@
+use std::io::{Read, Result, Seek, Write};
+use std::os::unix::prelude::FileExt;
+use std::path::PathBuf;
+use std::{ffi::OsString, fs::File, marker::PhantomData};
+
+use sha2::{Digest, Sha256};
+use tempfile::NamedTempFile;
+
+
+#[derive(Debug)]
+pub struct Blobstore<D = Sha256> {
+    base_dir: OsString,
+    tmp_dir: OsString,
+    digest: PhantomData<D>,
+}
+
+impl<D> Default for Blobstore<D> {
+    fn default() -> Self {
+        Blobstore {
+            base_dir: OsString::from("blobs"),
+            tmp_dir: OsString::from("tmp"),
+            digest: PhantomData,
+        }
+    }
+}
+
+impl<D> Blobstore<D> {
+    pub const fn new(base_dir: OsString, tmp_dir: OsString) -> Self {
+        Blobstore { base_dir, tmp_dir, digest: PhantomData }
+    }
+}
+
+impl<D: Digest> Blobstore<D> {
+    pub fn create(&mut self) -> Result<NewBlob<D>> {
+        Ok(NewBlob {
+            digest: D::new(),
+            file: NamedTempFile::new_in(&self.tmp_dir)?,
+        })
+    }
+
+    pub fn open(&self, name: String) -> Result<Blob> {
+        let blob_path = {
+            let (d, n) = name.split_at(2);
+            PathBuf::from(&self.base_dir).join(d).join(n)
+        };
+        Ok(Blob {
+            name,
+            file: File::open(blob_path)?
+        })
+    }
+
+    pub fn save(&mut self, new_blob: NewBlob<D>) -> Result<Blob> {
+        let name = hex::encode(new_blob.digest.finalize());
+
+        let mut hpath = std::path::PathBuf::new();
+        hpath.push(&self.base_dir);
+        let (dir, fname) = name.split_at(2);
+        hpath.push(dir);
+        let _ = std::fs::create_dir_all(hpath.clone());
+        hpath.push(fname);
+
+        Ok(Blob {
+            name,
+            file: new_blob.file.persist(hpath)?
+        })
+    }
+}
+
+#[derive(Debug)]
+pub struct Blob {
+    pub name: String,
+    file: File,
+}
+
+impl Blob {
+    pub fn read_at(&self, buf: &mut [u8], offset: u64) -> Result<usize> {
+        self.file.read_at(buf, offset)
+    }
+
+}
+
+impl Seek for Blob {
+    fn seek(&mut self, pos: std::io::SeekFrom) -> Result<u64> {
+        self.file.seek(pos)
+    }
+}
+
+impl Read for Blob {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.file.read(buf)
+    }
+}
+
+#[derive(Debug)]
+pub struct NewBlob<D: Digest = Sha256> {
+    digest: D,
+    file: NamedTempFile,
+}
+
+impl<D: Digest> Write for NewBlob<D> {
+    fn write(&mut self, bytes: &[u8]) -> Result<usize> {
+        let n = self.file.write(bytes)?;
+        self.digest.update(&bytes[..n]);
+        Ok(n)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.file.flush()
+    }
+}
