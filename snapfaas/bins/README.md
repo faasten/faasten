0056c53f4dbcec725ad9eeae6f5bbd15eaf1b556
Yue Tan <yuetan@cs.princeton.edu>
2022-06-02 14:24:09 -0400

    add binary sffs (buggy cat/write)

--- snapfaas/bins/README.md
+++ snapfaas/bins/README.md
@@ -1,63 +1,7 @@
-# snapctr
-
-Start SnapFaaS by running snapctr--the SnapFaaS controller. It starts a gateway
-for accepting requests and a pool of worker threads for handling requests. The
-gateway either waits for requests on a port (HTTPGateway) or reads requests
-from a file (FileGateway). Requests are represented as JSON strings. See
-`resources/example_requests.json` for examples.
-
-The gateway takes requests as input and output a `(Request, Channel)` tuple.
-`Request` is the data structure that represents a function invocation request.
-`Channel` is the response channel. This is a client TCP connetion with
-`HTTPGateway` and a `Sender` to a single response serializer thread with
-`FileGateway`.
-
-`snapctr` then takes the `(Request, Channel)` tuple and forward it to the
-worker pool. The worker pool consists of a pool of worker threads that are
-initialized and launched before the gateway starts accepting requests.  Pool
-size is `total memory/128`, where 128(MB) is the smallest VM size we support.
-
-All threads in the worker pool share the receiver end of a MPSC channel
-protected by a mutex (i.e., `Arc<Mutex<Receiver>>`) and the controller holds the
-sender end. All worker threads try to grab the lock on the receiver and then
-try to receive. Combining a MPSC channel with a mutex makes it simple to
-guarantee that all worker threads hang waiting for requests (not busy waiting),
-only one worker can receive at any given time and immediately after a worker
-finishes receiving, another worker can start receiving. See `Worker::new()`
-for more details.
-
-Once a worker receives a `(Request, Channel)` tuple, it tries to acquire a VM
-with the requested function loaded, forward it the request, wait for the VM to
-finish processing the request and respond, and finally send the response back
-to the client through the `Channel`.
-
-SnapFaaS uses Firecracker VMs and run them in processes separate from the
-controller. After processing a request, a VM does not shutdown. Instead it
-stays idle waiting for another request. Note that each VM in SnapFaaS has only
-a single function loaded. Therefore each VM can only handle a single type of
-requests.
-
-Worker threads (running inside the controller) are responsible for acquiring
-VMs to handle requests. To acquire a VM, a worker first tries to find an idle
-VM capable of handling the request. Such a VM needs to have the requested
-function loaded and is currently not processing another request. If the worker
-cannot find an suitable idle VM, it then tries to launch a VM with the right
-function loaded. It does this by checking if there's enough memory to launch a
-VM with the target function (different functions require VMs with different
-amount of memory) and then spawning a `firerunner` process with the right input
-parameters. If there's not enough memory left, the worker will try to kill idle
-VMs of other functions to free spaces. It first checks whether there's enough
-idle memory, if once freed, to launch its desired VM. If yes, it then kills
-those VMs and then launch its VM. If no, it fails the request and returns an
-error to client, indicating resource exhaustion.
-
-See `snapctr --help` for more details.
-
-# firerunner
-
-`firerunner` is a lightweight wrapper for Firecracker VMM. `snapctr` uses it to
-launch new VMs with specified functions loaded. We can also run `firerunner`
-independently to launch standalone VMs that are not managed by `snapctr`.
-
-See `firerunner --help` for more details.
-
+# List of Binaries 
+1. multivm: a FaaS backend server that runs multiple functions and receives requests from TCP connections.
+2. singlevm: a tool that runs a single function and receives line-delimited JSON requests from the stdin. 
+3. firerunner: a customized virtual machine manager based on firecracker that `multivm` and `singlevm` fork and run in a child process.
+4. sfdb: a tool that injects key-value pairs into the specified lmdb database.
+5. sfclient: a tool that sends requests over a TCP connection to `multivm`.
+6. sffs: a tool that interacts with the labeled file system atop a lmdb database.
---
+++ snapfaas/bins/sffs/main.rs
@@ -0,0 +1,211 @@
+#[macro_use(crate_version, crate_authors)]
+extern crate clap;
+use clap::{App, Arg, SubCommand};
+use labeled::dclabel::{self, DCLabel};
+use std::io::{Read, Write};
+
+use snapfaas::labeled_fs;
+
+fn input_to_dclabel(si_clauses: [Vec<&str>; 2]) -> DCLabel {
+    let mut components = Vec::new();
+    for clauses in si_clauses {
+        let component: dclabel::Component = if clauses[0].to_lowercase() == "true" {
+            true.into()
+        } else if clauses[0].to_lowercase() == "false" {
+            false.into()
+        } else {
+            let mut s_vec = Vec::new();
+            for clause in clauses {
+                let c: Vec<String> = clause.split(",").map(|s| s.to_lowercase()).collect();
+                s_vec.push(c);
+            }
+            s_vec.into()
+        };
+        components.push(component);
+    }
+    let secrecy = components.remove(0);
+    let integrity = components.remove(0);
+    DCLabel::new(secrecy, integrity)
+}
+
+fn input_to_endorsement(endorse: &str) -> DCLabel {
+    if endorse.to_lowercase() == "false" {
+        DCLabel::new(true, false)
+    } else if endorse.to_lowercase() == "true" {
+        DCLabel::new(true, true)
+    } else {
+        DCLabel::new(true, [[endorse.to_lowercase()]])
+    }
+}
+
+fn main() {
+    let cmd_arguments = App::new("sffs")
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about("a wrapper over the labeled_fs module.")
+        .subcommand(
+            SubCommand::with_name("ls")
+                .about("List the given directory")
+                .arg(Arg::with_name("PATH").index(1).required(true))
+        )
+        .subcommand(
+            SubCommand::with_name("cat")
+                .about("Ouput the given file to the stdout")
+                .arg(Arg::with_name("PATH").index(1).required(true))
+        )
+        .subcommand(
+            SubCommand::with_name("mkdir")
+                .about("Create a directory named by the given path with the given label")
+                .arg(Arg::with_name("PATH").index(1).required(true))
+                .arg(Arg::with_name("secrecy")
+                    .short("s")
+                    .long("secrecy")
+                    .multiple(true)
+                    .value_delimiter(";")
+                    .require_delimiter(true)
+                    .value_name("SECRECY CLAUSE")
+                    .required(true)
+                    .help("A DCLabel clause is a string of comma-delimited principals. Multiple clauses must be delimited by semi-colons."))
+                .arg(Arg::with_name("integrity")
+                    .short("i")
+                    .long("integrity")
+                    .multiple(true)
+                    .value_delimiter(";")
+                    .require_delimiter(true)
+                    .value_name("INTEGRITY CLAUSE")
+                    .required(true)
+                    .help("A DCLabel clause is a string of comma-delimited principals. Multiple clauses must be delimited by semi-colons."))
+                .arg(Arg::with_name("endorse")
+                    .short("e")
+                    .long("endorse")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Endorse the creation with the given principal"))
+        )
+        .subcommand(
+            SubCommand::with_name("mkfile")
+                .about("Create a file named by the given path with the given label")
+                .arg(Arg::with_name("PATH").index(1).required(true))
+                .arg(Arg::with_name("secrecy")
+                    .short("s")
+                    .long("secrecy")
+                    .multiple(true)
+                    .value_delimiter(";")
+                    .require_delimiter(true)
+                    .value_name("SECRECY CLAUSE")
+                    .required(true)
+                    .help("A DCLabel clause is a string of comma-delimited principals. Multiple clauses must be delimited by semi-colons."))
+                .arg(Arg::with_name("integrity")
+                    .short("i")
+                    .long("integrity")
+                    .multiple(true)
+                    .value_delimiter(";")
+                    .require_delimiter(true)
+                    .value_name("INTEGRITY CLAUSE")
+                    .required(true)
+                    .help("A DCLabel clause is a string of comma-delimited principals. Multiple clauses must be delimited by semi-colons."))
+                .arg(Arg::with_name("endorse")
+                    .short("e")
+                    .long("endorse")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Endorse the creation with the given principal"))
+        )
+        .subcommand(
+            SubCommand::with_name("write")
+                .about("Overwrite the given file with the data from the given file or the stdin")
+                .arg(Arg::with_name("PATH").index(1).required(true))
+                .arg(Arg::with_name("FILE")
+                    .short("f")
+                    .long("file")
+                    .takes_value(true)
+                    .value_name("FILE"))
+                .arg(Arg::with_name("endorse")
+                    .short("e")
+                    .long("endorse")
+                    .required(true)
+                    .takes_value(true)
+                    .help("Endorse the modification with the given principal"))
+        )
+        .get_matches();
+
+    let mut cur_label = DCLabel::public();
+    match cmd_arguments.subcommand() {
+        ("cat", Some(sub_m)) => {
+            if let Ok(data) = labeled_fs::read(sub_m.value_of("PATH").unwrap(), &mut cur_label) {
+                std::io::stdout().write_all(&data).unwrap();
+            } else {
+                eprintln!("Invalid path.");
+            }
+        },
+        ("ls", Some(sub_m)) => {
+            if let Ok(list) = labeled_fs::list(sub_m.value_of("PATH").unwrap(), &mut cur_label) {
+                let output = list.join("\t");
+                println!("{}", output);
+            } else {
+                eprintln!("Invalid path.");
+            }
+        },
+        ("mkdir", Some(sub_m)) => {
+            let path = std::path::Path::new(sub_m.value_of("PATH").unwrap());
+            let s_clauses: Vec<&str> = sub_m.values_of("secrecy").unwrap().collect();
+            let i_clauses: Vec<&str> = sub_m.values_of("integrity").unwrap().collect();
+            cur_label = input_to_endorsement(sub_m.value_of("endorse").unwrap());
+            match labeled_fs::create_dir(
+                path.parent().unwrap().to_str().unwrap(),
+                path.file_name().unwrap().to_str().unwrap(),
+                input_to_dclabel([s_clauses, i_clauses]),
+                &mut cur_label) {
+                Err(labeled_fs::Error::BadPath) => {
+                    eprintln!("Invalid path.");
+                },
+                Err(labeled_fs::Error::Unauthorized) => {
+                    eprintln!("Bad endorsement.");
+                },
+                Ok(()) => {},
+            }
+        },
+        ("mkfile", Some(sub_m)) => {
+            let path = std::path::Path::new(sub_m.value_of("PATH").unwrap());
+            let s_clauses: Vec<&str> = sub_m.values_of("secrecy").unwrap().collect();
+            let i_clauses: Vec<&str> = sub_m.values_of("integrity").unwrap().collect();
+            cur_label = input_to_endorsement(sub_m.value_of("endorse").unwrap());
+            match labeled_fs::create_file(
+                path.parent().unwrap().to_str().unwrap(),
+                path.file_name().unwrap().to_str().unwrap(),
+                input_to_dclabel([s_clauses, i_clauses]),
+                &mut cur_label) {
+                Err(labeled_fs::Error::BadPath) => {
+                    eprintln!("Invalid path.");
+                },
+                Err(labeled_fs::Error::Unauthorized) => {
+                    eprintln!("Bad endorsement.");
+                },
+                Ok(()) => {},
+            }
+        },
+        ("write", Some(sub_m)) => {
+            let data = sub_m.value_of("FILE").map_or_else(
+                || {
+                    let mut buf = Vec::new();
+                    std::io::stdin().read_to_end(&mut buf).unwrap();
+                    buf
+                },
+                |p| std::fs::read(p).unwrap()
+            );
+            cur_label = input_to_endorsement(sub_m.value_of("endorse").unwrap());
+            match labeled_fs::write(sub_m.value_of("PATH").unwrap(), data, &mut cur_label) {
+                Err(labeled_fs::Error::BadPath) => {
+                    eprintln!("Invalid path.");
+                },
+                Err(labeled_fs::Error::Unauthorized) => {
+                    eprintln!("Bad endorsement.");
+                },
+                Ok(()) => {},
+            }
+        },
+        (&_, _) => {
+            eprintln!("{}", cmd_arguments.usage());
+        }
+    }
+}
