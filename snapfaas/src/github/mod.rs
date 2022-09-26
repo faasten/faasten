78873db24d6a7bd420191b79bf639d64e10fe4a2
Yue Tan <yuetan@cs.princeton.edu>
2022-09-26 00:08:24 -0400

    update github label checking

--- snapfaas/src/github/mod.rs
+++ snapfaas/src/github/mod.rs
@@ -14,30 +14,38 @@ pub enum Error {
     BadHttpVerb,
     EmptyBody,
     BadRoute,
+    BadPayload,
 }
 
 type Result<T> = std::result::Result<T, Error>;
 
-fn check_label(route: &str, verb: syscalls::HttpVerb, cur_label: &mut DCLabel) -> Result<()> {
-    if let Some(suffix) = route.strip_prefix("/repos") {
+fn check_label(req: &syscalls::GithubRest, verb: syscalls::HttpVerb, cur_label: &mut DCLabel) -> Result<()> {
+    if let Some(suffix) = req.route.strip_prefix("/repos") {
         let v: Vec<&str> = suffix.split('/').collect();
         if v.len() < 2 {
             return Err(Error::BadRoute)
         }
-        let secrecy = format!("{}-{}-github", v[0], v[1]);
+        let secrecy = format!("{}:{}@github", v[0], v[1]);
         if verb == syscalls::HttpVerb::Get {
-            let gh_read = DCLabel::new([[secrecy]], [["github"]]);
+            let integrity = format!("{}:{}@github", v[0], v[1]);
+            let gh_read = DCLabel::new([[secrecy]], [[integrity]]);
             if !gh_read.can_flow_to(cur_label) {
                 *cur_label = gh_read.clone().lub(cur_label.clone());
             }
         } else if verb == syscalls::HttpVerb::Post && suffix.ends_with("generate") {
-            let integrity = format!("{}-github", v[0]);
+            // inspect the request body to find the owner of the repository to be created
+            let val: serde_json::Value = serde_json::from_str(req.body.as_ref().ok_or(Error::EmptyBody)?)
+                .map_err(|_| Error::BadPayload)?;
+            let obj = val.as_object().ok_or(Error::BadPayload)?;
+            let owner = obj["owner"].as_str().map(String::from).ok_or(Error::BadPayload)?;
+            let secrecy = format!("{}@github", owner);
+            let integrity = format!("{}@github", owner);
             let gh_write = DCLabel::new([[secrecy]], [[integrity]]);
             if !cur_label.can_flow_to(&gh_write) {
                 return Err(Error::Unauthorized);
             }
         } else {
-            let integrity = format!("{}-{}-github", v[0], v[1]);
+            let integrity = format!("{}:{}@github", v[0], v[1]);
             let gh_write = DCLabel::new([[secrecy]], [[integrity]]);
             if !cur_label.can_flow_to(&gh_write) {
                 return Err(Error::Unauthorized);
