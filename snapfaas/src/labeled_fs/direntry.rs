595c899efaaf2d782e0b55c8490ad337f841f033
Yue Tan <yuetan@cs.princeton.edu>
2022-10-05 22:16:06 -0400

    convert LabeledDirEntry.label field back

--- snapfaas/src/labeled_fs/direntry.rs
+++ snapfaas/src/labeled_fs/direntry.rs
@@ -13,44 +13,38 @@ pub enum DirEntry {
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct LabeledDirEntry {
-    label: Option<DCLabel>,
+    label: DCLabel,
     entry_type: DirEntry,
     uid: u64,
 }
 
 impl LabeledDirEntry {
     pub fn new(label: DCLabel, entry_type: DirEntry, uid: u64) -> Self {
-        Self { label: Some(label), entry_type, uid }
+        Self { label, entry_type, uid }
     }
 
     pub fn root() -> Self {
-        Self { label: Some(DCLabel::bottom()), entry_type: DirEntry::FacetedD, uid: 0u64 }
+        Self { label: DCLabel::bottom(), entry_type: DirEntry::FacetedD, uid: 0u64 }
     }
 
     /// raise label if necessary, and return the uid of the object
     pub fn unlabel(&self, cur_label: &mut DCLabel) -> &Self {
-        if let Some(label) = self.label.as_ref() {
-            if !label.can_flow_to(cur_label) {
-                *cur_label = label.clone().lub(cur_label.clone());
-            }
+        if !self.label.can_flow_to(cur_label) {
+            *cur_label = self.label.clone().lub(cur_label.clone());
         }
         &self
     }
 
     /// First read and raise label if needed then check if the writer can write
     pub fn unlabel_write_check(&self, cur_label: &mut DCLabel) -> Result<&Self> {
-        if let Some(label) = self.label.as_ref() {
-            // Enforcing that write implies read prevents low writers from writing high files
-            if !label.can_flow_to(cur_label) {
-                *cur_label = label.clone().lub(cur_label.clone());
-            }
-            if cur_label.can_flow_to(label) {
-                Ok(&self)
-            } else {
-                Err(Error::Unauthorized)
-            }
-        } else {
+        // Enforcing that write implies read prevents low writers from writing high files
+        if !self.label.can_flow_to(cur_label) {
+            *cur_label = self.label.clone().lub(cur_label.clone());
+        }
+        if cur_label.can_flow_to(&self.label) {
             Ok(&self)
+        } else {
+            Err(Error::Unauthorized)
         }
     }
 
