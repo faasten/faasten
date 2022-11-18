eca9db55958cbbad93bac2af11cd4596a7433785
Amit Aryeh Levy <amit@amitlevy.com>
2022-11-18 13:23:17 -0500

    Fix use of read_path to exclude leading string
    String formatted paths should not include an element for the (implicit)
    root directory

--- snapfaas/src/vm.rs
+++ snapfaas/src/vm.rs
@@ -493,7 +493,7 @@ impl Vm {
                     self.send_into_vm(result)?;
                 },
                 Some(SC::FsRead(req)) => {
-                    let value = fs::utils::read_path(&self.fs, req.path.split("/").map(String::from).collect()).ok().and_then(|entry| {
+                    let value = fs::utils::read_path(&self.fs, req.path.split("/").skip_while(|s| s.is_empty()).map(String::from).collect()).ok().and_then(|entry| {
                         match entry {
                             fs::DirEntry::Directory(_) => None,
                             fs::DirEntry::File(file) => self.fs.read(&file).ok()
@@ -506,7 +506,7 @@ impl Vm {
                     self.send_into_vm(result)?;
                 },
                 Some(SC::FsWrite(req)) => {
-                    let value = fs::utils::read_path(&self.fs, req.path.split("/").map(String::from).collect()).ok().and_then(|entry| {
+                    let value = fs::utils::read_path(&self.fs, req.path.split("/").skip_while(|s| s.is_empty()).map(String::from).collect()).ok().and_then(|entry| {
                         match entry {
                             fs::DirEntry::Directory(_) => None,
                             fs::DirEntry::File(file) => self.fs.write(&file, &req.data).ok()
@@ -521,7 +521,7 @@ impl Vm {
                 },
                 Some(SC::FsCreateDir(req)) => {
                     let label = proto_label_to_dc_label(req.label.clone().expect("label"));
-                    let value = fs::utils::read_path(&self.fs, req.base_dir.split("/").map(String::from).collect()).ok().and_then(|entry| {
+                    let value = fs::utils::read_path(&self.fs, req.base_dir.split("/").skip_while(|s| s.is_empty()).map(String::from).collect()).ok().and_then(|entry| {
                         match entry {
                             fs::DirEntry::Directory(dir) => {
                                 let newdir = self.fs.create_directory(label);
