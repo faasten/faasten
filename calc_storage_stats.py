38a360cad6aec1cad27cf5e21bb4d5ab9487ad3e
Yue Tan <yuetan@cs.princeton.edu>
2022-12-12 22:02:07 -0500

    update microbench to do loops inside sys-fs

--- calc_storage_stats.py
+++ calc_storage_stats.py
@@ -5,52 +5,44 @@ import json
 import statistics
 from collections import defaultdict
 
-d = Path('syscall-storage-stats')
-stats = []
-for statfile in d.iterdir():
-    with open(statfile) as f:
-        stats.append(json.loads(f.readline()))
-
-total_num = len(stats)
+with open('syscall-microbench') as f:
+    stat = json.loads(f.readline())
+total_num = 1000
+nano_to_micro = 1000
 
 e2e_lats = defaultdict(list)
-for stat in stats:
-    latencies = stat['response']['latencies']
-    for key in latencies:
-        e2e_lats[key].append(latencies[key])
+e2e_latencies = stat['response']['latencies']
+for key in e2e_latencies:
+    e2e_lats[key] = sum(e2e_latencies[key])/total_num/nano_to_micro
+print(e2e_lats)
 
-tot_lats = defaultdict(list)
-store_lats = defaultdict(list)
-serde_lats = defaultdict(list)
-label_lats = defaultdict(list)
-for stat in stats:
-    latencies = stat['syscall']
-    for key in latencies:
-        tot, store, serde, label = latencies[key]
-        tot_lats[key].append(tot['secs']*(10^9) + tot['nanos'])
-        store_lats[key].append(store['secs']*(10^9) + store['nanos'])
-        serde_lats[key].append(serde['secs']*(10^9) + serde['nanos'])
-        label_lats[key].append(label['secs']*(10^9) + label['nanos'])
-print('')
+tot_lats = {}
+store_lats = {}
+serde_lats = {}
+label_lats = {}
+latencies = stat['syscall']
+for key in e2e_latencies:
+    tot, store, serde, label = latencies[key]
+    tot_lats[key] = (tot['secs']*(10**9) + tot['nanos'])/total_num/nano_to_micro
+    store_lats[key] = (store['secs']*(10**9) + store['nanos'])/total_num/nano_to_micro
+    serde_lats[key] = (serde['secs']*(10**9) + serde['nanos'])/total_num/nano_to_micro
+    label_lats[key] = (label['secs']*(10**9) + label['nanos'])/total_num/nano_to_micro
 
-header = 'Label store serde label other'
+print(tot_lats)
+
+header = 'Label store serde label other rpc'
 writestoreops = {'write', 'create', 'delete'}
 largelines = []
+print('in microseconds')
 print(header)
 for key in tot_lats:
     textkey = '-'.join(key.split('_'))
-    store = sum(store_lats[key])/total_num//1000
-    serde = sum(serde_lats[key])/total_num//1000
-    label = sum(label_lats[key])/total_num//1000
-    tot = sum(tot_lats[key])/total_num//1000
+    store = store_lats[key]
+    serde = serde_lats[key]
+    label = label_lats[key]
+    tot = tot_lats[key]
+    e2e = e2e_lats[key]
     other = tot - store - serde - label
-    if key not in storeops:
-        line = ' '.join([textkey, str(store), str(serde), str(label), str(other)])
-        print(line)
-    else:
-        line = ' '.join([textkey, str(store/1000), str(serde/1000), str(label/1000), str(other/1000)])
-        largelines.append(line)
-
-print('milliseconds')
-for l in largelines:
-    print(l)
+    rpc = e2e - tot
+    line = ' '.join([textkey, str("%.2f"%store), str("%.2f"%serde), str("%.2f"%label), str("%.2f"%other), str("%.2f"%rpc)])
+    print(line)
--- functions/sys-fs/workload.py
+++ functions/sys-fs/workload.py
@@ -1,57 +1,61 @@
 import time
+from collections import defaultdict
 
 def handle(req, syscall):
-    response = {'latencies': {}, 'success': {}}
+    response = {'latencies': defaultdict(list), 'success': {}}
     latencies = response['latencies']
     success = response['success']
 
-    start = time.monotonic_ns()
-    user_facet = syscall.buckle_parse('user,user')
-    latencies['buckle_parse'] = time.monotonic_ns() - start
+    i = 1000
+    while i > 0:
+        i -= 1
+        start = time.monotonic_ns()
+        user_facet = syscall.buckle_parse('user,user')
+        latencies['buckle_parse'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    label = syscall.get_current_label()
-    latencies['get_current_label'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        label = syscall.get_current_label()
+        latencies['get_current_label'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    delegated = syscall.sub_privilege(['myapp','myfunc'])
-    latencies['sub_privilege'] = time.monotonic_ns() - start
- 
-    start = time.monotonic_ns()
-    success['list'] = syscall.fs_list([]) is not None
-    latencies['list'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        delegated = syscall.sub_privilege(['myapp','myfunc'])
+        latencies['sub_privilege'].append(time.monotonic_ns() - start)
+     
+        start = time.monotonic_ns()
+        success['list'] = syscall.fs_list([]) is not None
+        latencies['list'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    success['faceted_list'] = syscall.fs_faceted_list(['home']) is not None
-    latencies['faceted_list'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        success['faceted_list'] = syscall.fs_faceted_list(['home']) is not None
+        latencies['faceted_list'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    success['create'] = syscall.fs_createfile(['home', user_facet, 'file1'], label=user_facet)
-    latencies['create'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        success['create'] = syscall.fs_createfile(['home', user_facet, 'file1'], label=user_facet)
+        latencies['create'].append(time.monotonic_ns() - start)
 
-    data = bytes(req['args']['data'].encode('utf-8'))
-    start = time.monotonic_ns()
-    success['write'] = syscall.fs_write(['home', user_facet, 'file1'], data)
-    latencies['write'] = time.monotonic_ns() - start
+        data = bytes(req['args']['data'].encode('utf-8'))
+        start = time.monotonic_ns()
+        success['write'] = syscall.fs_write(['home', user_facet, 'file1'], data)
+        latencies['write'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    success['read'] = syscall.fs_read(['home', user_facet, 'file1']) is not None
-    latencies['read'] = time.monotonic_ns() - start
- 
-    start = time.monotonic_ns()
-    success['delete'] = syscall.fs_delete(['home', user_facet, 'file1'])
-    latencies['delete'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        success['read'] = syscall.fs_read(['home', user_facet, 'file1']) is not None
+        latencies['read'].append(time.monotonic_ns() - start)
+     
+        start = time.monotonic_ns()
+        success['delete'] = syscall.fs_delete(['home', user_facet, 'file1'])
+        latencies['delete'].append(time.monotonic_ns() - start)
+        
+        start = time.monotonic_ns()
+        label = syscall.taint_with_label(user_facet)
+        latencies['taint_with_label'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    label = syscall.taint_with_label(user_facet)
-    latencies['taint_with_label'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        label = syscall.endorse(delegated)
+        latencies['endorse'].append(time.monotonic_ns() - start)
 
-    start = time.monotonic_ns()
-    label = syscall.endorse(delegated)
-    latencies['endorse'] = time.monotonic_ns() - start
-
-    start = time.monotonic_ns()
-    label = syscall.declassify(label.secrecy)
-    latencies['declassify'] = time.monotonic_ns() - start
+        start = time.monotonic_ns()
+        label = syscall.declassify(label.secrecy)
+        latencies['declassify'].append(time.monotonic_ns() - start)
 
     return response
