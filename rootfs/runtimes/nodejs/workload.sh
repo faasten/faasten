f8a2d86821a09705dbe181acdf64e785a9397f98
Gongqi Huang <gongqih@hotmail.com>
2022-12-22 20:06:33 -0500

    update nodejs runtime

--- rootfs/runtimes/nodejs/workload.sh
+++ rootfs/runtimes/nodejs/workload.sh
@@ -4,4 +4,8 @@
 /usr/bin/ioctl
 
 /bin/mount -r /dev/vdb /srv
+
+# FIXME logfile cannot be created
+# npm_config_cache=/tmp/npm npm --version
+# npm config set cache /tmp/npm/ --loglevel=verbose
 NODE_PATH=$NODE_PATH:$(npm root --quiet -g) node /bin/runtime-workload.js
