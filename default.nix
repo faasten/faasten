ddbde7363ec517e28fc1a7a824e0588a3d6908a3
Amit Aryeh Levy <amit@amitlevy.com>
2023-03-22 09:31:30 -0400

    Switch from crate2nix to buildRustPackage

--- default.nix
+++ default.nix
@@ -1,10 +1,18 @@
-{ pkgs ? import <nixpkgs> {}, release ? true }:
+{ pkgs ? import <nixpkgs> {}} :
 
-with pkgs;
-builtins.mapAttrs (name: crate: crate.build.override {
-  crateOverrides = defaultCrateOverrides // {
-    prost-build = attrs: {
-      buildInputs = [ pkgs.protobuf ];
-    };
+pkgs.rustPlatform.buildRustPackage rec {
+  pname = "faasten";
+  version = "0.1.0";
+
+  src = ./.;
+
+  cargoSha256 = "sha256-aOXzWHiVHq7B/IuP/XBIl6RDunx1mUqVF4oGlLUfURA=";
+
+  nativeBuildInputs = [ pkgs.pkg-config pkgs.protobuf ];
+  buildInputs = [ pkgs.openssl ];
+
+  meta = {
+    description = "A user-centric function-as-a-service platform";
+    homepage = "https://github.com/faasten/faasten";
   };
-}) (import ./Cargo.nix { inherit pkgs release; }).workspaceMembers
+}
