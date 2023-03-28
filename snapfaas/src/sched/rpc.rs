e0584c446448bc21cec7bfd1fe43e58586842968
Yue Tan <yuetan@cs.princeton.edu>
2023-03-28 09:18:32 -0400

    fix non-string key type when serde

--- snapfaas/src/sched/rpc.rs
+++ snapfaas/src/sched/rpc.rs
@@ -1,16 +1,16 @@
+use serde::{Deserialize, Serialize};
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::net::TcpStream;
 use std::thread;
-use std::collections::HashMap;
-use std::collections::hash_map::DefaultHasher;
-use std::hash::{Hash, Hasher};
-use serde::{Serialize, Deserialize};
 
 use crate::fs::Function;
 
-use super::Error;
 use super::message;
-use super::message::{Request, Response, TaskReturn};
 use super::message::request::Kind as ReqKind;
+use super::message::{Request, Response, TaskReturn};
+use super::Error;
 
 /// ping
 pub fn ping(stream: &mut TcpStream) -> Result<Response, Error> {
@@ -40,9 +40,16 @@ pub fn get(stream: &mut TcpStream) -> Result<Response, Error> {
 }
 
 /// This method is for workers to return the result of a HTTP request
-pub fn finish(stream: &mut TcpStream, task_id: String, result: TaskReturn) -> Result<Response, Error> {
+pub fn finish(
+    stream: &mut TcpStream,
+    task_id: String,
+    result: TaskReturn,
+) -> Result<Response, Error> {
     let req = Request {
-        kind: Some(ReqKind::FinishTask(message::FinishTask { task_id, result: Some(result) })),
+        kind: Some(ReqKind::FinishTask(message::FinishTask {
+            task_id,
+            result: Some(result),
+        })),
     };
     message::write(stream, &req)?;
     let response = message::read_response(stream)?;
@@ -50,9 +57,12 @@ pub fn finish(stream: &mut TcpStream, task_id: String, result: TaskReturn) -> Re
 }
 
 /// This method is for workers to invoke a function
-pub fn labeled_invoke(stream: &mut TcpStream, labeled_invoke: message::LabeledInvoke) -> Result<(), Error> {
+pub fn labeled_invoke(
+    stream: &mut TcpStream,
+    labeled_invoke: message::LabeledInvoke,
+) -> Result<(), Error> {
     let req = Request {
-        kind: Some(ReqKind::LabeledInvoke(labeled_invoke))
+        kind: Some(ReqKind::LabeledInvoke(labeled_invoke)),
     };
     message::write(stream, &req)?;
     let _ = message::read_response(stream)?;
@@ -60,9 +70,12 @@ pub fn labeled_invoke(stream: &mut TcpStream, labeled_invoke: message::LabeledIn
 }
 
 /// This method is for workers to invoke a function
-pub fn unlabeled_invoke(stream: &mut TcpStream, unlabeled_invoke: message::UnlabeledInvoke) -> Result<(), Error> {
+pub fn unlabeled_invoke(
+    stream: &mut TcpStream,
+    unlabeled_invoke: message::UnlabeledInvoke,
+) -> Result<(), Error> {
     let req = Request {
-        kind: Some(ReqKind::UnlabeledInvoke(unlabeled_invoke))
+        kind: Some(ReqKind::UnlabeledInvoke(unlabeled_invoke)),
     };
     message::write(stream, &req)?;
     let _ = message::read_response(stream)?;
@@ -101,8 +114,10 @@ pub fn drop_resource(stream: &mut TcpStream) -> Result<(), Error> {
     Ok(())
 }
 
+#[serde_with::serde_as]
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ResourceInfo {
+    #[serde_as(as = "HashMap<serde_with::json::JsonString,_>")]
     pub stats: HashMap<Function, usize>,
     pub total_mem: usize,
     pub free_mem: usize,
