78d6ff2215e4f41ed6ec80f020a18ef47bcefccc
Yue Tan <yuetan@cs.princeton.edu>
2023-03-29 12:45:54 -0400

    remove apps

--- apps/grader/functions/post-comment/workload.py
+++
@@ -1,6 +0,0 @@
-def handle(req, syscall):
-    args = req['args']
-    meta = req['meta']
-    report = syscall.read(args['report'])
-    path = ['github', 'repos', meta['org'], meta['repo'], 'commits', meta['commit'], 'comments']
-    syscall.fs_write(path, report)
