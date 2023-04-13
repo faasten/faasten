3e07d4a803d9d24866e10039750fd3c391158135
Yue Tan <yuetan@cs.princeton.edu>
2023-04-13 19:30:41 -0400

    microbenchmark

--- functions/fsutil/workload.py
+++ functions/fsutil/workload.py
@@ -1,5 +1,6 @@
 import json
 import base64
+from time import time
 
 def handle(request, syscall):
     args = request['input']['args']
@@ -11,18 +12,21 @@ def handle(request, syscall):
         app_blob = request['input-blob']
         memory = args['memory']
         runtime = args['runtime']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_creategate(path, policy, app_blob, memory, runtime)
+        ret['finishing_time'] = time()
     elif op == 'create-redirect-gate':
         path = args['path']
         policy = args['policy']
         redirect_path = args['redirect_path']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_createredirectgate(path, policy, redirect_path)
+        ret['finishing_time'] = time()
     elif op == 'create-blob':
         blob = request['input-blob']
         path = args['path']
-        label = None
-        if 'label' in args:
-            label = syscall.buckle_parse(args['label'])
+        label = args['label']
+        ret['starting_time'] = time()
         if syscall.fs_linkblob(blob, path, label):
             ret['success'] = True
             #trigger_ret = None
@@ -31,9 +35,11 @@ def handle(request, syscall):
             #ret['trigger_status'] = trigger_ret
         else:
             ret['success'] = False
+        ret['finishing_time'] = time()
     elif op == 'create-file':
         path = args['path']
         label = args['label']
+        ret['starting_time'] = time()
         if syscall.fs_createfile(path, label):
             ret['success'] = True
             #trigger_ret = None
@@ -43,8 +49,10 @@ def handle(request, syscall):
             #ret['trigger_status'] = trigger_ret
         else:
             ret['success'] = False
+        ret['finishing_time'] = time()
     elif op == 'read-file':
         path = args['path']
+        ret['starting_time'] = time()
         v = syscall.fs_read(path)
         if v is None:
             ret['success'] = False
@@ -52,30 +60,40 @@ def handle(request, syscall):
             # return value type is bytes
             # encode it into utf-8 string
             ret['success'] = True
-            encoded = base64.b64encode(v)
+            encoded = base64.standard_b64encode(v)
             s = encoded.decode()
             ret['value'] = s
+        ret['finishing_time'] = time()
     elif op == 'write-file':
         path = args['path']
         data = args['data']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_write(path, data.encode('utf-8'))
+        ret['finishing_time'] = time()
     elif op == 'create-dir':
         path = args['path']
         label = args['label']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_createdir(path, label)
+        ret['finishing_time'] = time()
     elif op == 'list-dir':
         path = args['path']
+        ret['starting_time'] = time()
         v = syscall.fs_list(path)
         if v is None:
             ret['success'] = False
         else:
             ret['success'] = True
             ret['value'] = v
+        ret['finishing_time'] = time()
     elif op == 'create-faceted':
         path = args['path']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_createfaceted(path)
+        ret['finishing_time'] = time()
     elif op == 'list-faceted':
         path = args['path']
+        ret['starting_time'] = time()
         v = syscall.fs_faceted_list(path)
         if v is None:
             ret['success'] = False
@@ -84,7 +102,9 @@ def handle(request, syscall):
             ret['value'] = v
     elif op == 'delete':
         path = args['path']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_delete(path)
+        ret['finishing_time'] = time()
     elif op == 'create-service':
         path    = args['path']
         policy  = args['policy']
@@ -92,7 +112,35 @@ def handle(request, syscall):
         url     = args['url']
         verb    = args['verb']
         headers = args['headers']
+        ret['starting_time'] = time()
         ret['success'] = syscall.fs_createservice(path, policy, label, url, verb, headers)
+        ret['finishing_time'] = time()
+    elif op == 'declassify':
+        # TODO only for microbenchmark!
+        secrecy = args['secrecy']
+        parsed = syscall.buckle_parse(secrecy + ',T')
+        ret['starting_time'] = time()
+        syscall.declassify(parsed.secrecy)
+        ret['finishing_time'] = time()
+    elif op == 'endorse':
+        # TODO only for microbenchmark!
+        ret['starting_time'] = time()
+        syscall.endorse()
+        ret['finishing_time'] = time()
+    elif op == 'taint':
+        # TODO only for microbenchmark!
+        label = args['label']
+        label = syscall.buckle_parse(label)
+        ret['starting_time'] = time()
+        syscall.taint_with_label(label)
+        ret['finishing_time'] = time()
+    elif op == 'gen-blob':
+        # TODO only for microbenchmark!
+        data = b''
+        ret['starting_time'] = time()
+        with syscall.create_blob() as newblob:
+            newblob.finalize(data)
+        ret['finishing_time'] = time()
     else:
         ret['success'] = False
         ret['error'] = '[fsutil] unknown op'
---
+++ microbenchmark/run_microbenchmark.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+if [ -z "${KERNEL}" ]; then
+    echo 'KERNEL is unset'
+    exit 1
+fi
+
+if [ -z "${PYTHON}" ]; then
+    echo 'PYTHON is unset'
+    exit 1
+fi
+
+if [ -z "${FSUTIL}" ]; then
+    echo 'FSUTIL is unset'
+    exit 1
+fi
+
+tikv=$1
+
+ops=(create-dir read-dir delete-dir create-faceted read-faceted delete-faceted create-file write read-file delete-file label-taint label-endorse label-declassify gen-blob)
+
+rm -r stats
+rm -r output
+
+# setup output dir per op
+for op in "${ops[@]}"; do
+    mkdir -p stats/$op
+    mkdir -p output/$op
+done
+
+dd if=/dev/urandom of=4K bs=4K count=1 &>/dev/null
+for (( i=0; i<1; i++ )); do
+    for op in "${ops[@]}"; do
+        cat jsons/$op | singlevm $tikv --kernel $KERNEL --rootfs $PYTHON --appfs $FSUTIL \
+        --login faasten --stats stats/$op/stat$i \
+        > output/$op/output$i
+    done
+done
