a5a59a38dd747ff855674f559c88a8b32d865355
Amit Aryeh Levy <amit@amitlevy.com>
2023-06-16 15:54:21 -0400

    Rename docker_build back to mk_rtimage.sh

--- rootfs/mk_rtimage.sh
+++ rootfs/mk_rtimage.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
+function print_runtimes() {
+  echo -e "Available runtimes:"
+  for runtime_file in $(ls runtimes/)
+  do
+    echo -e "  * $(basename $runtime_file .sh)"
+  done
+}
+
+## Check command line argument length
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 RUNTIME"
+  print_runtimes
+  exit 1
+fi
+
 docker build -q -t faasten:base .
-make -C runtimes/$1
+make -C "runtimes/$1"
 
 CID=$(docker build -q runtimes/$1)
 cidfile=$(mktemp --dry-run)
